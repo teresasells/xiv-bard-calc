5cc738d8d212754b1c555ebca605717b4b8809ad
teresasells <teresa.nadolski@gmail.com>
2015-02-20 14:32:41 -0800

    Update biser.py
    just fucking around, looking at prunes and will remove dread gear i don't have RIP GG

--- biser.py
+++ biser.py
@@ -4,6 +4,8 @@ import numpy
 import math
 import gearcomparer
 
+#each item has the array associated with it [dex, acc, crit, det, skill speed, vitality]
+
 caps = numpy.array([0, 535, 0, 0, 0, 0])
 
 kirimuhat1 = [24, 25, 25, 18, 9, 28]
@@ -20,7 +22,7 @@ demonchest = [45, 0, 44, 22, 0, 52] # almost pruned
 aironworkschest = [50, 33, 0, 0, 47, 59] # destroyed by kirimu chest
 dreadchest = [50, 0, 0, 34, 33, 59]
 
-body = [kirimuchest, demonchest, dreadchest]
+body = [kirimuchest, demonchest, aironworkschest, dreadchest]
 
 kirimugloves1 = [24, 25, 25, 18, 9, 28]
 kirimugloves2 = [24, 18, 25, 18, 18, 28]
@@ -29,7 +31,7 @@ demongloves = [24, 0, 19, 0, 27, 32] #noo no no
 airongloves = [31, 0, 20, 21, 0, 36]
 dreadgloves = [31, 29, 0, 14, 0, 36]
 
-hands = [kirimugloves1, kirimugloves2, kirimugloves3, airongloves, dreadgloves]
+hands = [kirimugloves1, kirimugloves2, kirimugloves3, demongloves, airongloves, dreadgloves]
 
 arachnesash1 = [18, 19, 18, 13, 13, 21]
 arachnesash2 = [18, 18, 18, 13, 19, 21]
@@ -39,27 +41,27 @@ demonbelt = [21, 14, 0, 0, 20, 24] # yea no
 aironbelt = [23, 0, 15, 15, 0, 27]
 dreadbelt = [23, 22, 0, 11, 0, 27]
 
-waist = [arachnesash1, arachnesash2, arachnesash4, aironbelt, dreadbelt]
+waist = [arachnesash1, arachnesash2, arachnesash3, arachnesash4, demonbelt, aironbelt, dreadbelt]
 
 kirimupants = [39, 41, 41, 20, 0, 46]
 demonpants = [45, 0, 0, 22, 44, 52] # loser
 aironpants = [50, 47, 0, 0, 33, 59]
 dreadpants = [50, 33, 47, 0, 0, 59]
 
-legs = [kirimupants, aironpants, dreadpants]
+legs = [kirimupants, demonpants, aironpants, dreadpants]
 
 kirimufeet = [24, 25, 25, 18, 0, 28]
 demonfeet = [27, 0, 19, 19, 0, 32] # pruned again
 aironfeet = [31, 20, 29, 0, 0, 36]
 dreadfeet = [31, 29, 0, 0, 20, 36]
 
-feet = [kirimufeet, aironfeet, dreadfeet]
+feet = [kirimufeet, aironfeet, aironfeet, dreadfeet]
 
 scarf = [18, 19, 18, 12, 9, 0]
 aironchoak = [23, 15, 0, 15, 0, 0] # technically pooped on by scarf but who really has time for that?
 dreadchoak = [23, 22, 0, 0, 15, 0]
 
-necklace = [scarf, dreadchoak]
+necklace = [scarf, aironchoak, dreadchoak]
 
 aironearings = [23, 22, 0, 0, 15, 0]
 platear1 = [18, 18, 19, 12, 9, 0]
@@ -68,7 +70,7 @@ platear3 = [18, 0, 19, 13, 18, 0]
 dreadear = [23, 15, 0, 15, 0, 0] # pruned
 
 
-earrings = [platear1, platear2, platear3, aironearings]
+earrings = [dreadear, platear1, platear2, platear3, aironearings]
 
 platwrists1 = [18, 0, 19, 12, 19, 0] # pruned
 platwrists2 = [18, 9, 18, 12, 19, 0]
@@ -77,7 +79,7 @@ platwrists4 = [18, 19, 18, 0, 19, 0]
 aironwrists = [23, 22, 0, 0, 15, 0]
 dreadwrists = [23, 0, 22, 11, 0, 0]
 
-bracelets = [platwrists2, platwrists3, platwrists4, aironwrists, dreadwrists]
+bracelets = [platwrists1, platwrists2, platwrists3, platwrists4, aironwrists, dreadwrists]
 
 platring1 = [18, 0, 19, 13, 18, 0]
 platring2 = [18, 9, 18, 13, 18, 0]
@@ -87,7 +89,7 @@ dreadring = [23, 0, 0, 11, 22, 0] # pruned
 aironring = [23, 22, 15, 0, 0, 0]
 ironring = [21, 20, 14, 0, 0, 0] # wants to prune but thats only because it doesnt understand :(
 
-rings = [platring1, platring2, platring3, platring4, ironring, aironring] # accounting for unique rings is gonna be fun ._.
+rings = [platring1, platring2, platring3, platring4, ironring, aironring, dreadring] # accounting for unique rings is gonna be fun ._.
 
 allitems = [head, body, hands, waist, legs, feet, necklace, earrings, bracelets, rings, rings] # weapon
 allindex = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -95,6 +97,7 @@ allindex = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 def calc_effectivedex(gearset):
     seedweights = [1.0, 0, 0.339, 0.320, 0.161, 0]
     wd = [9.429]
+    #note that these weights are from http://www.reddit.com/r/ffxiv/comments/2q2tch/job_statweight_updates/ (22Dec2014)
     return numpy.sum(numpy.array(gearset)*numpy.array(seedweights))+52*wd[0]
 
 def pruneItems(itemset):
@@ -134,26 +137,26 @@ def increment(fullcombo, indexes):
 
 
 
-# print "head"
-# pruneItems(numpy.array(head))
-# print "body"
-# pruneItems(body)
-# print "hands"
-# pruneItems(numpy.array(hands))
-# print "waist"
-# pruneItems(numpy.array(waist))
-# print "legs"
-# pruneItems(numpy.array(legs))
-# print "feet"
-# pruneItems(numpy.array(feet))
-# print "necklace"
-# pruneItems(numpy.array(necklace))
-# print "earing"
-# pruneItems(numpy.array(earrings))
-# print "bracelet"
-# pruneItems(numpy.array(bracelets))
-# print "ring"
-# pruneItems(numpy.array(rings))
+print "head"
+pruneItems(numpy.array(head))
+print "body"
+pruneItems(body)
+print "hands"
+pruneItems(numpy.array(hands))
+print "waist"
+pruneItems(numpy.array(waist))
+print "legs"
+pruneItems(numpy.array(legs))
+print "feet"
+pruneItems(numpy.array(feet))
+print "necklace"
+pruneItems(numpy.array(necklace))
+print "earing"
+pruneItems(numpy.array(earrings))
+print "bracelet"
+pruneItems(numpy.array(bracelets))
+print "ring"
+pruneItems(numpy.array(rings))
 
 sumset(allitems, allindex)
 
